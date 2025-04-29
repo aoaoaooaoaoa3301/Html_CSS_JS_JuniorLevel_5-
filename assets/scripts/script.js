e4145aa883ce40e1422dc001d8566745227a270a
lookki <lookkiwtf@mail.ru>
2025-04-29 21:34:36 +0300

    add interactions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,11 @@
 const but_disign = document.querySelector(".but_disign");
 const icon_disign = but_disign.querySelector("img");
 var theme = "light";
-
 const body = document.querySelector("body");
 const nav_container = document.querySelector(".nav-container");
 const container = document.querySelectorAll(".container-item");
-const buts = document.querySelectorAll(".but_light");
+const but_remove = document.querySelectorAll(".but_light");
+const but_filtr = document.querySelectorAll("#filtr-but");
 const text = document.querySelectorAll(".text");
 
 const but_toggle = document.querySelectorAll("#toggle-but");
@@ -24,21 +24,60 @@ but_disign.addEventListener('click', function(){
 
         changeTheme("dark");
     }
-
 });
+but_filtr.forEach(el =>{
+    el.addEventListener('click', function(){
+        if(el.className == "but_light_disable"){
+            el.classList.add("but_light_enable");
+            el.classList.remove("but_light_disable");
+            var but_used = el.dataset.filtr;
+        }
+        else if(el.className == "but_dark_disable"){
+            el.classList.add("but_dark_enable");
+            el.classList.remove("but_dark_disable");
+            var but_used = el.dataset.filtr;
+        }
+        but_filtr.forEach(i =>{
+            if(i.dataset.filtr != but_used){
+                if(i.className == "but_light_enable"){
+                    i.classList.add("but_light_disable");
+                    i.classList.remove("but_light_enable");
+                }
+                else if(i.className == "but_dark_enable"){
+                    i.classList.add("but_dark_disable");
+                    i.classList.remove("but_dark_enable");
+                }
+            }
+        });
 
+        changeFiltr(el);
+    });
+});
 but_toggle.forEach(el =>{
     el.addEventListener('click', function(){
-        if(el.className == "but_toggle_active"){
-            el.classList.add("but_toggle_inactive");
-            el.classList.remove("but_toggle_active");
-            
+        var parent = el.parentElement.parentElement.parentElement;
+        if(el.className == "but_toggle_active_light"){
+            el.classList.add("but_toggle_inactive_light");
+            el.classList.remove("but_toggle_active_light");
+            parent.dataset.vision = "disable";
         }
-        else if(el.className == "but_toggle_inactive"){
-            el.classList.add("but_toggle_active");
-            el.classList.remove("but_toggle_inactive")
-            
+        else if(el.className == "but_toggle_inactive_light"){
+            el.classList.add("but_toggle_active_light");
+            el.classList.remove("but_toggle_inactive_light")
+            parent.dataset.vision = "enable";
         }
+
+        else if(el.className == "but_toggle_active_dark"){
+            el.classList.add("but_toggle_inactive_dark");
+            el.classList.remove("but_toggle_active_dark");
+            parent.dataset.vision = "disable";
+        }
+        else if(el.className == "but_toggle_inactive_dark"){
+            el.classList.add("but_toggle_active_dark");
+            el.classList.remove("but_toggle_inactive_dark")
+            parent.dataset.vision = "enable";
+        }
+        
     });
 });
 
@@ -57,9 +96,29 @@ function changeTheme(color){
             el.style.color ='rgba(255, 255, 255, 0.57)';
         });
 
-        buts.forEach(el => {
+        but_filtr.forEach(el => {
+            if(el.className == "but_light_enable"){
+                el.classList.add('but_dark_enable');
+                el.classList.remove('but_light_enable');
+            }
+            else if(el.className == "but_light_disable"){
+                el.classList.add('but_dark_disable');
+                el.classList.remove('but_light_disable');
+            }
+        });
+        but_remove.forEach(el => {
             el.classList.add('but_dark');
             el.classList.remove('but_light');
+        });
+        but_toggle.forEach(el => {
+            if(el.className == "but_toggle_inactive_light"){
+                el.classList.add('but_toggle_inactive_dark');
+                el.classList.remove('but_toggle_inactive_light');
+            }
+            else if(el.className == "but_toggle_active_light") {
+                el.classList.add('but_toggle_active_dark');
+                el.classList.remove('but_toggle_active_light');
+            }
         });
         but_disign.id = 'disign-but_dark';
 
@@ -78,12 +137,62 @@ function changeTheme(color){
             el.style.color ='rgba(0, 0, 0, 0.568)';
         });
         
-        buts.forEach(el => {
+        but_remove.forEach(el => {
             el.classList.add('but_light');
             el.classList.remove('but_dark');
+        });
+        but_filtr.forEach(el => {
+            if(el.className == "but_dark_enable"){
+                el.classList.add('but_light_enable');
+                el.classList.remove('but_dark_enable');
+            }
+            else if(el.className == "but_dark_disable"){
+                el.classList.add('but_light_disable');
+                el.classList.remove('but_dark_disable');
+            }
+        });
+
+        but_toggle.forEach(el => {
+            if(el.className == "but_toggle_inactive_dark"){
+                el.classList.add('but_toggle_inactive_light');
+                el.classList.remove('but_toggle_inactive_dark');
+            }
+            else if(el.className == "but_toggle_active_dark") {
+                el.classList.add('but_toggle_active_light');
+                el.classList.remove('but_toggle_active_dark');
+            }
         });
         but_disign.id = 'disign-but_light';
 
         nav_container.style.background = 'white'
     }
 }
+
+function changeFiltr(el){
+    if(el.dataset.filtr == "all"){
+        container.forEach(i =>{
+            i.style.display = "flex";
+        });
+    }
+    else if(el.dataset.filtr == "active"){
+        container.forEach(i =>{
+            if(i.dataset.vision == "enable"){
+                i.style.display = "flex";
+            }
+            else if(i.dataset.vision == "disable"){
+                i.style.display = "none";
+            }
+        });
+    }
+    else if(el.dataset.filtr == "inactive"){
+        container.forEach(i =>{
+            if(i.dataset.vision == "disable"){
+                i.style.display = "flex";
+            }
+            else if(i.dataset.vision == "enable"){
+                i.style.display = "none";
+            }
+        });
+    }
+}
+
